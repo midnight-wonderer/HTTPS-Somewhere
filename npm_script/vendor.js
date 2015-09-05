76d84face1c1f8dd8e94b1897073a2d93b659105
MidnightWonderer <midnight_w@gmx.com>
2015-09-05 10:10:52 +0700

    modify vendor script

--- npm_script/vendor.js
+++ npm_script/vendor.js
@@ -1,6 +1,8 @@
 var shell = require('gulp-shell');
 var download = shell.task([
-  'rm -rf vendor/*',
-  'curl https://oss.maxcdn.com/rxjs/3.1.2/rx.min.js -o vendor/rx.min.js',
+  'rm -rf source/vendor/*',
+  'mkdir -p source/vendor/script',
+  'curl https://oss.maxcdn.com/rxjs/3.1.2/rx.min.js -o source/vendor/script/rx.min.js',
+  'curl https://oss.maxcdn.com/requirejs/2.1.20/require.min.js -o source/vendor/script/require.min.js'
 ]);
 download();
